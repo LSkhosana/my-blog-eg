76f3f688aa0f68435105767501010cd53ec6efd3
Lesedi <skhosanalesedi888@gmail.com>
2024-07-18 12:23:49 +0200

    bcryptjs add

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,19 @@
+import {db} from "../db"
 export const register = (req,res)=>{
+
+    // Check existing user
+    const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+    db.query(q, [req.body.email, req.body.name], (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data.length) return res.status(409).json("User already exists!");
+
+        // Hash the password and create a user
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+    });
+    
 
 }
 
