2785adfa799b94014b1603ba134ee1402b727ff0
Lesedi <skhosanalesedi888@gmail.com>
2024-07-18 12:10:22 +0200

    post methods add

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from "express"
 import postRoutes from "./routes/posts.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/user.js"
+
 
 const app = express()
 
 app.use(express.json())
 app.use("/api/posts", postRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+
 
 // app.get("/test", (req,res)=>{
 //     res.json("It works")
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { addPost } from "../controllers/post.js"
+// import { addPost } from "../controllers/post.js"
 
 const router = express.Router()
 
 // using this router you can make any request 
-router.get("/test", addPost)
+// router.get("/test", addPost)
 
 export default router
