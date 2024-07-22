204b532fe66ebb5099184de71ab0c55bbcc60c2d
Lesedi <skhosanalesedi888@gmail.com>
2024-07-22 14:04:47 +0200

    final push

--- api/index.js
+++ api/index.js
@@ -1,23 +1,34 @@
-import express from "express"
-import postRoutes from "./routes/posts.js"
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/user.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/posts", postRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
+const upload = multer({ storage });
 
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
-// app.get("/test", (req,res)=>{
-//     res.json("It works")
-// })
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
-app.listen(8800,()=>{
-    console.log("Connected!")
-})
+app.listen(8800, () => {
+  console.log("Connected!");
+});
