46d779d79b9795b601fd13735727a5cebcce33cf
Lesedi <skhosanalesedi888@gmail.com>
2024-07-22 13:35:26 +0200

    posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,18 @@
-import express from "express"
-// import { addPost } from "../controllers/post.js"
+import express from "express";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.js";
 
-const router = express.Router()
+const router = express.Router();
 
-// using this router you can make any request 
-// router.get("/test", addPost)
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
 
-export default router
+export default router;
